3cd10fd9e92288b94806eeaa6770df0210260b58
Galiman1593 <eunesiopinto01@gmail.com>
2024-07-05 19:10:15 -0300

    Create checar_operador.js

---
+++ AULA_5/AULA_5/checar_operador.js
@@ -0,0 +1,23 @@
+// 1 diz qua foi o operador passado e na sua saída o que ele fez
+
+const operadorEscolido = "pogChamp";
+
+if (operadorEscolido === "+") {
+  console.log(
+    "o operador de mais é responsavel por somar inteiros ou concatenar strings "
+  );
+} else if (operadorEscolido === "-") {
+  console.log("subtrai valores inteiros");
+} else if (operadorEscolido === "*") {
+  console.log("multiplica valores inteiros");
+} else if (operadorEscolido === "poggers") {
+  console.log("fica flinstons");
+} else if (operadorEscolido === "/") {
+  console.log("divide valores inteiros");
+} else if (operadorEscolido === "pogChamp") {
+  console.log("omegalul total, wal");
+} else if (operadorEscolido === "==") {
+  console.log("operador de igualdade, compara de o VALOR é igual");
+} else if (operadorEscolido === "===") {
+  console.log("Operador de idêntico, compara se o TIPO e o VALOR são iguais");
+}
