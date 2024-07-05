7b0d130029c63aae48a432ed130379b921ae367e
Galiman1593 <eunesiopinto01@gmail.com>
2024-07-05 19:31:08 -0300

    Update checar_operador.js

--- AULA_5/AULA_5/checar_operador.js
+++ AULA_5/AULA_5/checar_operador.js
@@ -30,3 +30,19 @@ else if (operadorEscolido === "==") {
 else if (operadorEscolido === "!==") {
     console.log("operador de DIFERENTE, compara se é diferente do valor");
 }
+else if (operadorEscolido === "<=") {
+    console.log("Operador de MENOR ou IGUAL, verifica se o valor e MENOR ou IGUAL");
+}
+else if (operadorEscolido === ">=") {
+    console.log("Operador de MAIOR ou IGUAL, verifica se o valor e MAIOR ou IGUAL");
+}
+//logico
+else if (operadorEscolido === "&&") {
+    console.log("Operador de 'É', todas as condições devem ser verdadeiras");
+}
+else if (operadorEscolido === "||") {
+    console.log("Operador de OU, pelo menos uma das condições devem ser veradeiras");
+}
+else if (operadorEscolido === "!") {
+    console.log("operador 'NOT' ou oposição, valor contratio ou oposto");
+}
