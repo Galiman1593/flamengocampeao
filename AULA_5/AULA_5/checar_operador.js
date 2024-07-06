c44f448facb6518c8bc0fd12db0dc34ea9dc831d
Galiman1593 <eunesiopinto01@gmail.com>
2024-07-05 21:33:17 -0300

    Feat: Organização
    organizei os codigos

--- AULA_5/AULA_5/checar_operador.js
+++ AULA_5/AULA_5/checar_operador.js
@@ -17,32 +17,34 @@ if (operadorEscolido === "+") {
   console.log("divide valores inteiros");
 } else if (operadorEscolido === "pogChamp") {
   console.log("omegalul total, wal");
-}
-else if (operadorEscolido === "%") {
-    console.log("operador de resto, faz a divisao mas exibe o resto ao em vez do resultado");
+} else if (operadorEscolido === "%") {
+  console.log(
+    "operador de resto, faz a divisao mas exibe o resto ao em vez do resultado"
+  );
 }
 //coparação
 else if (operadorEscolido === "==") {
   console.log("operador de igualdade, compara de o VALOR é igual");
 } else if (operadorEscolido === "===") {
   console.log("Operador de idêntico, compara se o TIPO e o VALOR são iguais");
-}
-else if (operadorEscolido === "!==") {
-    console.log("operador de DIFERENTE, compara se é diferente do valor");
-}
-else if (operadorEscolido === "<=") {
-    console.log("Operador de MENOR ou IGUAL, verifica se o valor e MENOR ou IGUAL");
-}
-else if (operadorEscolido === ">=") {
-    console.log("Operador de MAIOR ou IGUAL, verifica se o valor e MAIOR ou IGUAL");
+} else if (operadorEscolido === "!==") {
+  console.log("operador de DIFERENTE, compara se é diferente do valor");
+} else if (operadorEscolido === "<=") {
+  console.log(
+    "Operador de MENOR ou IGUAL, verifica se o valor e MENOR ou IGUAL"
+  );
+} else if (operadorEscolido === ">=") {
+  console.log(
+    "Operador de MAIOR ou IGUAL, verifica se o valor e MAIOR ou IGUAL"
+  );
 }
 //logico
 else if (operadorEscolido === "&&") {
-    console.log("Operador de 'É', todas as condições devem ser verdadeiras");
-}
-else if (operadorEscolido === "||") {
-    console.log("Operador de OU, pelo menos uma das condições devem ser veradeiras");
-}
-else if (operadorEscolido === "!") {
-    console.log("operador 'NOT' ou oposição, valor contratio ou oposto");
+  console.log("Operador de 'É', todas as condições devem ser verdadeiras");
+} else if (operadorEscolido === "||") {
+  console.log(
+    "Operador de OU, pelo menos uma das condições devem ser veradeiras"
+  );
+} else if (operadorEscolido === "!") {
+  console.log("operador 'NOT' ou oposição, valor contratio ou oposto");
 }
