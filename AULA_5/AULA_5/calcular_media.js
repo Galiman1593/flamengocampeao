cdedd10ebaec1edf1406f757978897d0555afbdb
Galiman1593 <eunesiopinto01@gmail.com>
2024-07-05 21:21:34 -0300

    feat: ficou_midia_agora
    foram adicionadas ponto e virgula no final dos códigos e flamengo

--- AULA_5/AULA_5/calcular_media.js
+++ AULA_5/AULA_5/calcular_media.js
@@ -1,7 +1,7 @@
 // Escreva um algoritmo que calcule a média da soma das notas, e diga se um aluno passou ou não.
 // 1 - Você deve definir quatro constantes sendo elas primeiraNota, segundaNota, terceiraNota, quartaNota.
 // 2 - Atribuir uma nota de 0 até 10 para cada uma das notas Ex: const primeiraNota = 5;
-// 3 - Criar uma constante chamada media, e botar a média a atribuir o valor 5, Ex: const media = 5; 
+// 3 - Criar uma constante chamada media, e botar a média a atribuir o valor 5, Ex: const media = 5;
 // 4 - Criar uma constante notaFinal que calcule a média da seguinte forma:
 // const notaFinal = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
 // 5 - Vai ser necessária utilizar apenas um único IF e ELSE no código.
@@ -10,19 +10,20 @@
 // 8 - Caso contrário imprimir a saída com console.log("O aluno não passou");
 // 9 - No final executar com node 'nome_do_arquivo.js' para vermos a execução no terminal
 // 10 - Para checar com total certeza, coloque um console.log(notaFinal) no final do código e verifique se o resultado bate com o desejado.
+//flamengo campeão mundial 2024
 
-const media = 5
+const media = 5;
 
-const primeiraNota = 5
+const primeiraNota = 5;
 
-const segundaNota = 8
+const segundaNota = 8;
 
-const terceiraNota = 3
+const terceiraNota = 3;
 
-const quartaNota = 7
+const quartaNota = 7;
 
-const notaFinal = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4
+const notaFinal = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
 
 if (notaFinal >= media) {
-    console.log("você passou pitbull");
+  console.log("você passou pitbull");
 } else console.log("tu não passou");
