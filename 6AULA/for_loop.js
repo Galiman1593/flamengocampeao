d105c0e185624d15cd25bbb61a5ae616e7da8aff
Galiman1593 <eunesiopinto01@gmail.com>
2024-07-08 19:32:48 -0300

    feat:ARQUIVO_SAFADO

---
+++ 6AULA/for_loop.js
@@ -0,0 +1,10 @@
+for(contador = 0; contador <= 10; contador++){
+    console.log("flamengo campeao de 201", contador);
+    console.log("flamengo campeao de 2020!");
+}
+
+
+for(contador = 0; contador<= 10; contador++){
+    console.log("valor da gasolina:", contador);
+    console.log("valor do etanol ta 10 mereis");
+}
