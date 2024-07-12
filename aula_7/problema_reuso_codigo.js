cf1243d2a8544dd8e3d0c936e6b57e59905bba1f
MegatronFlamengista <eunesiopinto01@gmail.com>
2024-07-12 19:16:37 -0300

    feat:area_do_circulo

---
+++ aula_7/problema_reuso_codigo.js
@@ -0,0 +1,9 @@
+//passar em algoritimos: a formula para calcular a area de um circulo
+// 1 passo- criar uma constante chamada pi e atribuir o valor de 3.14
+// 2-criar uma constante chamada raio e atribuir o valor 5
+// 3-criar uma constante chamada area e atribuir pi *(raio*raio)
+
+const pi = 3.14
+const raio = 5 
+const area = pi *(raio*raio)
+
