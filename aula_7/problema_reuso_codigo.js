821a4500147d87637d178b01dfe9a92017d934e7
MegatronFlamengista <eunesiopinto01@gmail.com>
2024-07-12 19:52:07 -0300

    feat:Algoritimo_de_fomula_triangulo

--- aula_7/problema_reuso_codigo.js
+++ aula_7/problema_reuso_codigo.js
@@ -1,9 +1,70 @@
-//passar em algoritimos: a formula para calcular a area de um circulo
+//1 passar em algoritimos: a formula para calcular a area de um circulo
 // 1 passo- criar uma constante chamada pi e atribuir o valor de 3.14
 // 2-criar uma constante chamada raio e atribuir o valor 5
 // 3-criar uma constante chamada area e atribuir pi *(raio*raio)
 
-const pi = 3.14
-const raio = 5 
-const area = pi *(raio*raio)
+//2 mudanças de ultima hora, descobri que necessito reutilizar esta formula em outros treixos do softwaer
+
+//FUNÇÃO
+
+function CalcularAreaCirculo(raio) {
+  const pi = 3.14;
+  return area = pi * (raio * raio);
+}
+
+console.log(CalcularAreaCirculo(30));
+
+
+//3 foi solicitado para que voc\ê faça o mesmo proscedimento feito com a formula da area do circulo, soque dessa vez vc vai fazer com a area do triangulo
+// 1- pesquizar a formula do triangulo
+// 2- definir uma função chamada CalcularAreaTriangulo
+// 3- colocar um console.log(CalcularAreaTriangulo(10));
+
+
+
+function CalcularAreaTriangulo(base, altura){
+    return area = base * altura / 2;
+}
+
+console.log(CalcularAreaTriangulo(10, 10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
